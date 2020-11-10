5b6da87dda1986dc74f2db7a50776ace3414b5cb
capn <jamesgreen.311@gmail.com>
2020-11-10 10:56:34 -0500

    Create artist's art work slide

--- process.js
+++ process.js
@@ -4,6 +4,7 @@
 //  copy artist info and image slide
 //  update title slide with show name
 //  update artist info and image slide with show data
+//  remove artist info slide template from deck
 function process() {
     const templateSlideDeckId = "118eqgpPvbCnGMk_gktMQNtAwhFS4mJ-bezEA0hSrw8Y";
 
@@ -14,6 +15,27 @@ function process() {
     const newSlideDeck = SlidesApp.openById(fileCopy.getId());
     const slides = newSlideDeck.getSlides();
     const pSlide = slides[0];
+    const artistSlide = slides[1];
     let [title] = pSlide.getPageElements();
     title.asShape().getText().setText(exhibitName);
+
+    exhibitEntries.forEach(el => {
+        let newSlide = newSlideDeck.appendSlide(artistSlide);
+        let [artist, title, medium, size, price, photo] = newSlide.getPageElements();
+        
+        artist.asShape().getText().setText(`${el[2]} ${el[3]}`);
+        title.asShape().getText().setText(el[6]);
+        medium.asShape().getText().setText(el[9]);
+        size.asShape().getText().setText(`${el[7]}\" x ${el[8]}\"`);
+        price.asShape().getText().setText(`\$${el[10]}`);
+        try {
+            let image = DriveApp.getFileById(el[12]).getBlob();
+            photo.asImage().replace(image);
+        } catch(error) {
+            // bad image id
+            console.error("Bad Image Id");
+        }
+    });
+
+    artistSlide.remove();
 };
